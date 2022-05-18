4c45fe5cfdf1edf118f1fcbbae7ac0b37edd318f
Claire Kim <kclaire56@yahoo.com>
2022-05-17 20:46:25 -0700

    remove puctuation function

--- project/helpers.py
+++ project/helpers.py
@@ -1,9 +1,19 @@
 from distutils import text_file
+from string import punctuation
 from wordcloud import STOPWORDS, WordCloud
 import re
 import matplotlib.pyplot as plt
 
+
+class Utils:
+    punctuation = r'[\.,#\-!?:/]'
+
+
 def get_file_contents(txt_file):
-    with open(text_file) as f:
+    with open(txt_file) as f:
         contents =  f.readlines()
     return contents
+
+def remove_punctuation(text):
+    revised_text = re.sub(Utils.punctuation, '',text)
+    return revised_text
